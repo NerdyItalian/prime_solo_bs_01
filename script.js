f17be344a8a414bc22bf0826bd9b761e8231819c
NerdyItalian <atava8384@gmail.com>
2015-04-28 13:33:28 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-var apikey = ''; // Put your API key here
-
+var apikey = 'e27fa347f5134d05d053b02acb00c1153f892615';
+var userSearch;
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
-    console.log(results);
+	var count = 0;
+	var cell = '';
+	for (var i = 0; i < 9; i++){
+	    var games = results[i];
+	    	var otherInfo = '<br><div class="bonus"><br><strong>Game Description: </strong>' + games.deck + '</div>'
+	    	cell += '<div class="col-md-4 well"><img class="img-thumbnail hidden-xs hidden-sm" src="' + games.image.icon_url + '"/><br><p class="lead">'  + games.name + '</p>' + otherInfo +'<button class="btn btn-sm btn-success removeBtn">Remove</button></div>';
+			count++
+			if (count == 3){
+				$(".searchResults").append('<div class="row">' + cell + '</div>');
+				count = 0;
+				cell = '';
+		};
+	}
+	console.log(results);
 }
 
-$(document).ready(function() {
 
-	// Start the search here!
+
+//search("batman");
+
+$(document).ready(function() {
+	$(".searchBtn").on("click", function(){
+		userSearch = $("#searchField").val();
+		search(userSearch);
+	});
+
+	$(".searchResults").on("click", ".removeBtn", function(){
+
+		$(this).parent().remove();
+	})
 	
 });
 
